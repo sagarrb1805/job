121241384bd3bc71213c6d13e3954a9b4e8f302b
Sagar R.B <sagarrb1805@gmail.com>
2023-04-13 13:21:17 +0530

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,40 @@
 import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+
+
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+
 import { LoginComponent } from './components/login/login.component';
 
+
+
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+
+declarations: [
+
+ AppComponent,
+
+  LoginComponent
+
+],
+
+imports: [
+
+ BrowserModule,
+
+  AppRoutingModule
+
+ ],
+
+ providers: [],
+ bootstrap: [AppComponent]
+
 })
+
 export class AppModule { }
